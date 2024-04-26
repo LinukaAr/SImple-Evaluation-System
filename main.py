a004f5238c8f877e67e8118fb6f20f1667e64d4f
Linuka Arambawela <linukaarambawela@gmail.com>
2024-04-26 15:25:56 +0530

    basic Structure

--- main.py
+++ main.py
@@ -1,55 +1,24 @@
 import random
 
-opetators = ['+', '-', '*', '/']
-min_operann = 1
-max_operann = 20
+operators = ['+', '-', '*']
+min_operand = 1
+max_operand = 20
+total_problems = 3
 
 def generate_question():
-    first = random.randint(min_operann, max_operann)
-    second = random.randint(min_operann, max_operann)
-    operator = random.choice(opetators)
-    question = str(first) + operator + str(second)
-    print(question)
-    return question
-
-def get_input():
-    return print(int(input("Enter your answer: ")))
-    
-
-
-def main():
-    generate_question()
-    import random
-
-opetators = ['+', '-', '*']
-min_operann = 1
-max_operann = 20
-
-def generate_question():
-    first = random.randint(min_operann, max_operann)
-    second = random.randint(min_operann, max_operann)
-    operator = random.choice(opetators)
-    question = str(first) + operator + str(second)
-    answer = eval(question) # evaluates a string expression and returns the result
-    print(question)
-    print(answer)
+    first = random.randint(min_operand, max_operand)
+    second = random.randint(min_operand, max_operand)
+    operator = random.choice(operators)
+    question = f"{first} {operator} {second}"
+    answer = eval(question)
     return question, answer
 
-def get_input():
-    return print(int(input("Enter your answer: ")))
-    
-def validation(answer):
-    user_answer = get_input()
-    if user_answer == answer:
-        print("Good job!")
-    else:
-        print("wrong anwser! Try again.")
-            
-
-
-def main():
-    answer = generate_question()
-    validation(answer)
-
-if __name__ == "__main__":
-    main()
+for i in range(total_problems):
+    question, answer = generate_question()
+    while True:
+        user_answer = int(input(f"Q{i+1}. {question} = "))
+        if user_answer == answer:
+            print("Correct!")
+            break
+        else:
+            print("Incorrect. Try again.")
